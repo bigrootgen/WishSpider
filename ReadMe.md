80813c30827f314394c2ec27ae901e05bf205619
heyuehui <bigroot@bupt.eud.cn>
2019-02-16 12:53:28 +0800

    Now we can download and process data from wish

---
+++ ReadMe.md
@@ -0,0 +1,38 @@
+依赖库：
+
+1）selenium
+
+solution: python -m pip install selenium
+
+2) requests
+
+solution: python -m pip install requests
+
+安装方法：
+
+1) 将selenium目录拷贝到D盘根目录下
+
+2）安装Python3.7并设置好相应的环境变量
+
+3）见依赖库
+
+4）安装firefox最新版
+
+运行方法：
+
+在WishSpider目录里打开cmd并运行sel.py（python sel.py)
+
+运行完后，相关数据保存在transaction.csv中
+
+打开Excel新建表格，选择数据，选择自文本，选择transaction.csv
+
+1）选择下一步
+
+2）取消tab键，选择逗号
+
+3）选择下一步
+
+4）选择完成
+
+5）选择确定
+
---
+++ sel.py
@@ -0,0 +1,43 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import requests
+import json
+import csv
+
+driver = webdriver.Firefox(executable_path='D:\selenium\geckodriver.exe')
+driver.get("http://wishpost.wish.com/login")
+elem = driver.find_element_by_name("username")
+elem.send_keys("dennishe023@hotmail.com")
+elem = driver.find_element_by_name("password")
+elem.send_keys("hxd697112")
+elem.send_keys(Keys.RETURN)
+time.sleep(1)
+cookies = driver.get_cookies()
+driver.close()
+
+cookie_s = ''
+for i in cookies:
+    cookie_s = cookie_s + i['name'] + '=' + i['value'] +';'
+cookie_s = cookie_s[0:-1]
+
+url = 'https://wishpost.wish.com/api/payment/get-cash-flow'
+data = {}
+data['endpoint'] = 'payment/get-cash-flow'
+data['callback'] = 'renderCashFlowReport'
+data['offset'] = '0'
+data['count'] = '1000'
+data['num_results'] = '0'
+data['currency'] = 'CNY'
+data['range'] = '30d'
+headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0',
+'Cookie':cookie_s}
+rep = requests.post(url, data, headers = headers)
+content = json.loads(rep.text)
+
+data = content['data']
+cash_flow_list = data['cash_flow_list']
+with open('./transaction.csv', 'w') as f:
+    writer = csv.writer(f)
+    for i in cash_flow_list:
+        writer.writerow(i.values())
